647eb20d8c324f30f6d4ea32d7694b478fd5e481
Artem Moroz <deusestmaleficus@gmail.com>
2023-09-06 18:25:29 +0700

    remake function in dicts.py

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,10 +1,4 @@
 def get_val(collection, key, default='git'):
-    print_key = key
-    if len(collection) == 0:
-        return default
+    return collection.get(key)
 
-    elif collection.get(print_key) is not None:
-        return collection.get(print_key)
-
-    return default
 
